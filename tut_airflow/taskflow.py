2dbeb2d9ebb84dabdaf99f66dd3a924bb0c38141
csprl-nowigence <caleb.spraul@nowigence.com>
2022-03-10 17:00:00 -0500

    dag_id not required, i guess.

--- tut_airflow/taskflow.py
+++ tut_airflow/taskflow.py
@@ -9,7 +9,6 @@ from airflow.decorators import dag, task
 
 
 @dag(
-    dag_id='my_tutorial_taskflow_api_etl',
     schedule_interval=None,
     start_date=pendulum.datetime(2022, 1, 1, tz='UTC'),
     catchup=False,
